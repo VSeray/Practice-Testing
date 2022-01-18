761c48c2cf166255907ba718a3a1bdc0cfa76814
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-01-18 14:41:06 +0200

    Create sting and reverse elements

--- src/script.js
+++ src/script.js
@@ -1,12 +1,21 @@
-import _ from 'lodash';
-import './style.css';
-function component() {
-  const element = document.createElement('div');
+const stringLength = (string) => {
+  const c = string.length;
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+  if (c > 0 && c <= 10) {
+      return c;
+  } else {
+      throw new Error('String must be at least be 1 character long and not more longer than 10 characters');
+  }
 }
 
-document.body.appendChild(component());
+const reverseString = (string) => {
+  return string.split('').reverse().join('');
+}
+
+const capitalize = (string) => {
+  const val = string.charAt(0).toUpperCase() + string.slice(1);
+
+  return val;
+}
+
+module.exports = {stringLength, reverseString, capitalize};
